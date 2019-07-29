711bc41d3d4ebeea27865d91eb729dffc29e22de
Javi Gomez <rtomyj@gmail.com>
2019-07-29 12:54:28 -0500

    Create colorize.sh

---
+++ colorize.sh
@@ -0,0 +1,29 @@
+function yellow_output()
+{
+        printf "\033[1;33m"
+}
+
+function red_output()
+{
+        printf "\033[1;31m"
+}
+
+function green_output()
+{
+        printf "\033[0;32m"
+}
+
+function cyan_output()
+{
+        printf "\033[1;36m"
+}
+
+function normal_output()
+{
+        printf "\033[0m"
+}
+
+function orange_output()
+{
+        printf "\033[0;33m"
+}
